9b32635132cc1db9853ca7e141b022b641113d4b
tantran1011 <tantran10112000@gmail.com>
2025-04-26 20:43:25 +0700

    Fixing CORS

--- app/main.py
+++ app/main.py
@@ -12,11 +12,17 @@ load_dotenv()
 
 app = FastAPI()
 
+origins = [
+    "http://127.0.0.1:5173",         
+    "http://localhost:5173",          
+    "https://your-frontend-production.com",
+]
+
 app.mount("/static", StaticFiles(directory="app/static"), name='static')
 app.add_middleware(SessionMiddleware, secret_key = os.getenv("SECRET_KEY"))
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
