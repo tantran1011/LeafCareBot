977884e546480112ca48a60147410bc8b7173a5a
tantran1011 <tantran10112000@gmail.com>
2025-04-26 18:56:52 +0700

    Fix logic Login

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -22,13 +22,18 @@ def register(user: Register, db: Session = Depends(get_db)):
 
 @router.post('/login', response_model=UserRespone)
 def login(request: Request, user: Login, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.username == user.username).first()
-    if not user:
+    db_user = db.query(User).filter(User.username == user.username).first()
+    if not db_user:
         auth_logger.warning("Invalid Credentials")
         raise HTTPException(status_code=401, detail="Invalid credentials")
+    
+    if db_user.password != user.password:
+        auth_logger.warning("Invalid Password")
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    
     auth_logger.info(f"{user.username} Sucessfull Login")
-    request.session["user_id"] = user.id
-    return UserRespone(username=user.username)
+    request.session["user_id"] = db_user.id
+    return UserRespone(username=db_user.username)
 
 
 @router.get('/users')
